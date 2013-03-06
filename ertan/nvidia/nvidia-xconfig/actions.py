8a179af99a991b43e39793476b059d92f3db0443
Ertan Güven <ertangvn@gmail.com>
2013-03-06 14:48:43 +0200

    nvidia xorg package

---
+++ ertan/nvidia/nvidia-xconfig/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dodir("/usr/bin")
+    autotools.install("ROOT=%s" % get.installDIR())
