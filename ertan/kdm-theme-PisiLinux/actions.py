7c3fcd6eccf901f47f783746d33d456b33f840f7
Demiray <mdemiray@msn.com>
2013-03-13 19:56:25 +0200

    Update actions.py
    bug cleaned

--- ertan/kdm-theme-PisiLinux/actions.py
+++ ertan/kdm-theme-PisiLinux/actions.py
@@ -10,5 +10,5 @@ from pisi.actionsapi import kde4
 WorkDir = "./"
 
 def install():
-    pisitools.insinto("%s/kdm/themes/" % kde4.appsdir, "pisilinux-default-kdm", "Pisilinux Default Kdm")
+    pisitools.insinto("%s/kdm/themes/" % kde4.appsdir, "Pisilinux Default Kdm", "Pisilinux Default Kdm")
 
