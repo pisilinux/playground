e01da40f7d9710c307975aa0f993ad2755b9a7b2
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2013-03-14 10:16:22 +0200

    “pspp”

---
+++ ayhanyalcinsoy/pspp/actions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+#from pisi.actionsapi import pisitools
+
+# if pisi can't find source directory, see /var/pisi/pspp/work/ and:
+# WorkDir="pspp-"+ get.srcVERSION() +"/sub_project_dir/"
+
+def setup():
+    autotools.configure("--with-libplot \
+                         --with-libncurses \
+                         --enable-nls \
+                         --enable-static \
+                         --disable-rpath \
+                         --with-x \
+                         --with-gui")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+# Take a look at the source folder for these file as documentation.
+#    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
+
+# If there is no install rule for a runnable binary, you can 
+# install it to binary directory.
+#    pisitools.dobin("pspp")
+
+# You can use these as variables, they will replace GUI values before build.
+# Package Name : pspp
+# Version : 0.7.9
+# Summary : ÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ°statistik analiz programÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ±
+
+# For more information, you can look at the Actions API
+# from the Help menu and toolbar.
+
+# By PiSiDo 2.0.0
