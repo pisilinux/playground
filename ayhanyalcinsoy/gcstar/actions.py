87a2c5abb72a6daea0dfa75dd311417e1f6319df
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2013-03-20 01:42:47 +0200

    gcstar

--- ayhanyalcinsoy/gcstar/actions.py
+++ ayhanyalcinsoy/gcstar/actions.py
@@ -6,19 +6,22 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
+# from pisi.actionsapi import shelltools
 
 # if pisi can't find source directory, see /var/pisi/gcstar/work/ and:
 WorkDir="./gcstar"
 
-def setup():
-    autotools.configure()
-
-def build():
-    autotools.make("./install")
+def install():
+    pisitools.dobin("/bin/gcstar", "/bin/")
+    pisitools.dolib("/lib/gcstar", "/usr/lib/")
+    pisitools.doman("/man/gcstar.1")
+    pisitools.domove("/share/applications/ ","/share/applications")
+    pisitools.domove("/share/gcstar/","/share/")
+    
+# def build():
+#      autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
+#    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
 
