c9a617c4351fa65113c0be9672d329f788a92421
namso-01 <namso-01@hotmail.it>
2013-03-27 18:12:43 +0200

    perl-cddb-file + perl-mp3-info + perl-mp3-tag + perl-net-freedb + perl-ogg-header-pureperl depoya alındı.

--- ayhanyalcinsoy/gcstar/actions.py
+++ ayhanyalcinsoy/gcstar/actions.py
@@ -6,13 +6,11 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-# from pisi.actionsapi import shelltools
-
-# if pisi can't find source directory, see /var/pisi/gcstar/work/ and:
-WorkDir="./gcstar"
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure()
+    shelltools.system("./install")
+    #autotools.configure()
 #     pisitools.dobin("./bin/gcstar", "/bin")
 #     pisitools.dolib("/lib/gcstar", "usr/lib/")
 #     pisitools.doman("./man/gcstar.1")
@@ -20,7 +18,7 @@ def setup():
 #     pisitools.domove("./share/gcstar","usr/share")
     
 def build():
-      autotools.make("./install")
+    shelltools.system("./install text")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
