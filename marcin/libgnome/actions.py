007bee5bcb6da8a607c867de27a47db393ac8b1e
marcin- <marcin.bojara@gmail.com>
2013-03-30 14:21:53 +0100

    fixes for glib-2.36.0

---
+++ marcin/libgnome/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    pisitools.dosed("libgnome/", "DG_DISABLE_DEPRECATED", namePattern = "Makefile\.(am|in)", deleteLine = True)
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-static \
+                         --disable-schemas-install \
+                         --disable-esd")
+    pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("NEWS", "README", "ChangeLog", "AUTHORS")
