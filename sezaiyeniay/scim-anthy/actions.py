613a0ca977b105a34669af0142347e668d0da1de
sezaiyeniay <sezaiyeniay@pisilinux.org>
2013-04-24 05:01:35 +0300

    scim-anthy

--- sezaiyeniay/scim-anthy/actions.py
+++ sezaiyeniay/scim-anthy/actions.py
@@ -5,6 +5,7 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 
 def setup():
@@ -14,6 +15,6 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("COPYING","INSTALL","README")
