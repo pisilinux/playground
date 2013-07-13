b42b161a6f58c0da0de7f68c79f1f46d80bfc7a1
namso-01 <namso-01@hotmail.it>
2013-07-14 00:22:59 +0300

    adeskbar new package

---
+++ namso-01/adeskbar/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+def install():
+    pisitools.insinto("/usr/share/adeskbar", "src/*")
+    pisitools.insinto("/usr/share/applications", "adeskbar.desktop")
+    pisitools.insinto("/usr/bin/", "adeskbar.sh", "adeskbar")
+    pisitools.insinto("/usr/share/pixmaps", "src/images/adeskbar.png")
+    pisitools.dodoc("README")
