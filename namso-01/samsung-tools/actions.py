ed7ce162cf34ade58ca52361ec25b9319fd0ef0e
namso-01 <namso-01@hotmail.it>
2013-07-21 22:46:51 +0300

    samsung-tools

---
+++ namso-01/samsung-tools/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def install():
+    pisitools.dosed("samsung-tools.py", "/usr/bin/python2", "/usr/bin/python")
+    pisitools.dosed("samsung-tools-preferences.py", "/usr/bin/python2", "/usr/bin/python")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "LICENSE", "README", "TODO")
