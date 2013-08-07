9f36b87b26e80eb08b1756c00f901cd2c4bfe51b
BlackSEA- <blacksea8393@gmail.com>
2013-08-07 13:23:04 +0300

    kozmetik

--- blacksea/pysdm/actions.py
+++ blacksea/pysdm/actions.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="desura"
-NoStrip = ["/"]
+def setup():
+    autotools.configure()
 
-#def setup():
-    
-#def build():
+def build():
+    autotools.make()
 
-def install():  
-    pisitools.insinto("/opt/desura/", "desura", "desura")
-    shelltools.chmod("%s/opt/desura/desura" %get.workDIR())
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+
