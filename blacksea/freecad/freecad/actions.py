0b3e1f3afda1abdc0ef3e5e617b99ec94d5b4a44
BlackSEA- <blacksea8393@gmail.com>
2013-08-24 22:00:41 +0300

    duzenleme

--- blacksea/freecad/freecad/actions.py
+++ blacksea/freecad/freecad/actions.py
@@ -9,23 +9,32 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "freecad-0.13.1830"  
+WorkDir = "freecad-0.13.1830"
 
 def setup():
     shelltools.makedirs("%s/%s/build" % (get.workDIR(),WorkDir))
     shelltools.cd("%s/%s/build" % (get.workDIR(),WorkDir))
-    cmaketools.configure("-DOCC_INCLUDE_DIR:PATH=/usr/include/occ/ ", sourceDir = "..")
+    cmaketools.configure("-DOCC_INCLUDE_DIR:PATH=/usr/include/occ/ \ "
+			,"-DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python \ "
+			, sourceDir = "..")
                          
-                         # -DOCC_INCLUDE_DIR:PATH=/usr/include/occ/ 
+                         # -DOCC_INCLUDE_DIR:PATH=/usr/include/occ/
 
 
 def build():
     shelltools.cd("%s/%s/build" % (get.workDIR(),WorkDir))
     cmaketools.make()
 
+
+#('CXXFLAGS="%s -fpermissive" \
+
+#CFLAGS="%s"' % (get.CXXFLAGS(), get.CFLAGS()))
+
+
+
+
 def install():
     libtools.libtoolize()
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dosym("/usr/lib/freecad/FreeCAD", "/usr/bin/FreeCAD") 
+    pisitools.dosym("/usr/lib/freecad/FreeCAD", "/usr/bin/FreeCAD")
     pisitools.dosym("/usr/lib/freecad/FreeCADCmd", "/usr/bin/FreeCADCmd")
-  
