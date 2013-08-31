39fbea3a020571e8c1f3d3347a6badf99c3e41b5
marcin- <marcin.bojara@gmail.com>
2013-08-31 20:53:33 +0200

    add readline

---
+++ pisi-ng/system/base/readline/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    #Force link to ncurses instead of tinfo, which we don't have, will be needed when we use as-needed ;)
+    pisitools.dosed("support/shobj-conf", "^(SHLIB_LIBS=)", "\\1-lncurses")
+    if get.buildTYPE() == "x86_64": pisitools.cflags.add("-fPIC")
+
+    pisitools.dosed("support/shobj-conf", "\-Wl,\-rpath,\$\(libdir\)\s")
+    autotools.configure("--with-curses \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s install" % get.installDIR())
+
+    if get.buildTYPE() == "emul32": return
+
+    pisitools.removeDir("/usr/bin")
+
+    pisitools.dohtml("doc/")
+    pisitools.dodoc("CHANGELOG", "CHANGES", "README", "USAGE", "NEWS")
