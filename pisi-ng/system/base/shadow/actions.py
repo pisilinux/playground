adaa18aed855cfc2c56d7b1f3657c89473427372
obsoleteman <tulliana@gmail.com>
2013-08-31 18:56:10 +0300

    add shadow

---
+++ pisi-ng/system/base/shadow/actions.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    sed -i 's@DICTPATH.*@DICTPATH\t/lib/cracklib/pw_dict@' \
+        etc/login.defs
+    autotools.configure("--enable-shadowgrp \
+                         --without-selinux \
+                         --with-audit \
+                         --with-libcrack \
+                         --with-libpam \
+                         --enable-nls \
+                         --with-group-name-max-length=32 \
+                         --disable-shared")
+def build():
+    # Rebuild gmo catalogs
+    autotools.make("-C po update-gmo")
+
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/etc/", "etc/login.access")
+    shelltools.chmod("%s/etc/login.access" % get.installDIR(), 0600)
+
+    pisitools.insinto("/etc/", "etc/limits")
+    shelltools.chmod("%s/etc/limits" % get.installDIR(), 0644)
+
+    # groups come from coreutils package
+    pisitools.remove("/usr/share/man/man1/groups.1")
+    pisitools.remove("/bin/groups")
+
+    # Conflicts with man-pages
+    pisitools.remove("/usr/share/man/man3/getspnam.3")
+    pisitools.remove("/usr/share/man/man5/passwd.5")
+
+    pisitools.dodoc("ChangeLog","README","NEWS")
+
