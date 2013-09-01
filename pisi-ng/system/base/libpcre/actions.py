5589768d261749532132589a8e8d7f82df31a661
obsoleteman <tulliana@gmail.com>
2013-09-01 17:18:33 +0300

    add some syste.devel packs and deps

---
+++ pisi-ng/system/base/libpcre/actions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import libtools
+
+def setup():
+    options = "--enable-jit \
+               --enable-pcretest-libreadline \
+               --enable-pcre32 \
+               --enable-pcre16 \
+               --enable-utf \
+               --enable-unicode-properties \
+               --enable-cpp \
+               --docdir=/%s/%s \
+               --disable-static" % (get.docDIR(), get.srcNAME())
+
+    if get.buildTYPE() == "emul32":
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CXXFLAGS())
+        shelltools.export("PKG_CONFIG_LIBDIR", "/usr/lib32/pkgconfig")
+
+    libtools.libtoolize("--copy --force")
+    autotools.autoreconf("-vif")
+    autotools.configure(options)
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("-j1 check")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
