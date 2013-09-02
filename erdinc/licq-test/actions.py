4f929c61ecc00a04830560ed6ff208ea4709e3af
obsoleteman <tulliana@gmail.com>
2013-09-02 05:38:56 +0300

    licq: mucks

--- erdinc/licq-test/actions.py
+++ erdinc/licq-test/actions.py
@@ -5,34 +5,25 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import cmaketools
-from pisi.actionsapi import qt4
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-dirs=["build","plugins"]
 
 def setup():
     shelltools.makedirs("build")
-    for dir in dirs:
-        shelltools.cd(dir)
-        cmaketools.configure("-DUSE_OPENSSL=ON -DCMAKE_INSTALL_PREFIX=/usr" ,sourceDir="..")
-        shelltools.cd("..")
+    shelltools.cd("build")
+    cmaketools.configure("-DUSE_OPENSSL=ON -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_PLUGINS=ON", sourceDir="..")
+
 
 def build():
     shelltools.cd("build")
     cmaketools.make()
-    shelltools.cd("..")
-
-    shelltools.cd("plugins")
-    cmaketools.make()
 
 
 def install():
-    for dir in dirs:
-        shelltools.cd(dir)
-        cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-        shelltools.cd("..")
+    shelltools.cd("build")
+    cmaketools.install()
 
+    shelltools.cd("..")
     pisitools.dodoc("LICENSE", "README")
