4e5fce7508b19e1da0adfe505e96c7926c2d4c5a
BlackSEA- <blacksea8393@gmail.com>
2013-09-26 09:39:08 +0300

    flareget v2.1.8

--- blacksea/flareget/actions.py
+++ blacksea/flareget/actions.py
@@ -12,19 +12,26 @@ from pisi.actionsapi import get
 WorkDir = "."
 def setup():
   
-      shelltools.cd("flareget_2.0-15_x86_64(stable)_rpm")
+      shelltools.cd("flareget_2.1-18_x86_64(stable)_rpm")
+      shelltools.system("rpm2targz flareget-2.1-18.x86_64.rpm")
+      shelltools.system("tar xfvz flareget-2.1-18.x86_64.tar.gz")
+      shelltools.cd("..")
+      shelltools.cd("flareget_2.1-18_x86_64(stable)_rpm/Browser Integration/Opera(version < 13 only)")
       shelltools.system("rpm2targz flaremonitor-1.0-2.x86_64.rpm")
-      shelltools.system("rpm2targz flareget-2.0-15.x86_64.rpm")
       shelltools.system("tar xfvz flaremonitor-1.0-2.x86_64.tar.gz")
-      shelltools.system("tar xfvz flareget-2.0-15.x86_64.tar.gz")
+      
 
 def install():
-      shelltools.cd("flareget_2.0-15_x86_64(stable)_rpm")
-      pisitools.insinto("/lib/","./lib/*")
+      shelltools.cd("flareget_2.1-18_x86_64(stable)_rpm")
       pisitools.insinto("/usr/bin/","./usr/bin/*")
-      pisitools.insinto("/usr/sbin/","./usr/sbin/*")
       pisitools.insinto("/usr/share/","./usr/share/*")
       pisitools.insinto("/usr/lib/","./usr/lib64/*")
+      shelltools.cd("..")
+      shelltools.cd("flareget_2.1-18_x86_64(stable)_rpm/Browser Integration/Opera(version < 13 only)")
+      pisitools.insinto("/lib/","./lib/*")
+      pisitools.insinto("/usr/sbin/","./usr/sbin/*")
+      pisitools.insinto("/usr/share/","./usr/share/*")
+     
       shelltools.chmod("%s/usr/bin/flare-grab" % get.installDIR(), 0755)
       shelltools.chown("%s/usr/bin/flare-grab" % get.installDIR(), gid="users")
       shelltools.chmod("%s/usr/bin/flareget" % get.installDIR(), 0755)
