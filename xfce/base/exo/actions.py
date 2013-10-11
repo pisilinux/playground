5af75205e3164fc4ff33face0c51612700f7c707
suvari <suvarice@gmail.com>
2013-10-11 12:07:01 +0300

    add exo

---
+++ xfce/base/exo/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--sysconfdir=/etc \
+                         --libexecdir=/usr/lib/xfce4 \
+                         --localstatedir=/var \
+                         --disable-static \
+                         --disable-debug")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "ChangeLog", "NEWS", "README", "THANKS", "TODO")
