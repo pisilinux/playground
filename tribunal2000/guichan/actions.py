c841d4d0608b073e53bdb5f58b13d43fd0dae8dc
tribunal2000 <tribunal2000@gmail.com>
2013-11-23 01:24:17 +0400

    guichan lib

--- tribunal2000/guichan/actions.py
+++ tribunal2000/guichan/actions.py
@@ -3,35 +3,33 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 #from pisi.actionsapi import pisitools
 
-# if pisi can't find source directory, see /var/pisi/guichan/work/ and:
-# WorkDir="guichan-"+ get.srcVERSION() +"/sub_project_dir/"
+# if pisi can't find source directory, see /var/pisi/__package_name__/work/ and:
+# WorkDir="__package_name__-"+ get.srcVERSION() +"/sub_project_dir/"
 
 def setup():
-    cmaketools.configure("-DCMAKE_BUILD_TYPE=release", installPrefix="/usr")
+    autotools.configure()
 
 def build():
-    cmaketools.make()
+    autotools.make()
 
 def install():
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
 # Take a look at the source folder for these file as documentation.
 #    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
 
 # If there is no install rule for a runnable binary, you can 
 # install it to binary directory.
-#    pisitools.dobin("guichan")
+#    pisitools.dobin("__package_name__")
 
 # You can use these as variables, they will replace GUI values before build.
-# Package Name : guichan
-# Version : 0.8.2
-# Summary : guichan
+# Package Name : __package_name__
+# Version : __version__
+# Summary : __summary__
 
 # For more information, you can look at the Actions API
 # from the Help menu and toolbar.
-
-# By PiSiDo 2.0.0
