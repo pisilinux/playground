f923dae34c7e9c559d3546c2ed4d312ca08861ce
pisiganesh <iyer.gb.linux@gmail.com>
2013-12-05 00:13:31 +0600

    Added Firefox Nightly 28.0.1a

---
+++ pisiganesh/Firefox_Nightly/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "firefox"
+
+def install():
+    shelltools.cd("..")
+    pisitools.dodir("/opt")
+    pisitools.insinto("/opt","firefox")
+    
+# By PiSiDo 2.0.0
