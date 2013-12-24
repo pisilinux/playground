1a4fab018272db6c11b7fac5b8e45da500d0a6ee
obsoleteman <tulliana@gmail.com>
2013-12-25 00:51:19 +0200

    elltube, still does nor work

--- alihanozturk/elltube/actions.py
+++ alihanozturk/elltube/actions.py
@@ -7,45 +7,16 @@
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 WorkDir="elltube-0.3"
 
+
+
 def build():
-    cmaketools.make()
+    pass
+    
 
 def install():
-    pisitools.insinto("usr/share/doc/elltube", "debian/copyright")
-    pisitools.insinto("usr/share/doc/elltube", "debian/changelog")
-    pisitools.insinto("usr/share/applications/", "elltube.desktop")
-    pisitools.insinto("usr/share/elltube/", "elltube.py")
-    pisitools.insinto("usr/share/elltube/atom/", "atom/__init__.py")
-    pisitools.insinto("usr/share/elltube/atom/", "atom/service.py")
-    pisitools.insinto("usr/share/elltube/gdata/", "gdata/__init__.py")
-    pisitools.insinto("usr/share/elltube/gdata/", "gdata/auth.py")
-    pisitools.insinto("usr/share/elltube/gdata/", "gdata/service.py")
-    pisitools.insinto("usr/share/elltube/gdata/", "gdata/test_data.py")
-    pisitools.insinto("usr/share/elltube/gdata/", "gdata/urlfetch.py")
-    pisitools.insinto("usr/share/elltube/gdata/geo/", "gdata/geo/__init__.py")
-    pisitools.insinto("usr/share/elltube/gdata/media/", "gdata/media/__init__.py")
-    pisitools.insinto("usr/share/elltube/gdata/youtube/", "gdata/youtube/__init__.py")
-    pisitools.insinto("usr/share/elltube/gdata/youtube/", "gdata/youtube/service.py")
-    pisitools.insinto("usr/share/elltube/img/", "img/cancel.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/elltube.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/exit.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/folder.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/license.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/login.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/open.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/paste.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/save.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/settings.png")
-    pisitools.insinto("usr/share/elltube/img/", "img/translators.png")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/ca.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/de.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/es.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/fr.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/it.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/pl.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/pt_BR.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/sr.qm")
-    pisitools.insinto("usr/share/elltube/locale/", "locale/sr@Latn.qm")
+    shelltools.system("make DESTDIR=%s PREFIX=/usr install" % get.installDIR())
+    #pisitools.dobin("/usr/bin/elltube")
