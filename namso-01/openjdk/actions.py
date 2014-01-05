94503eeaed2077397afdaecee081db7551faab96
marcin- <marcin.bojara@gmail.com>
2014-01-05 12:59:29 +0100

    openjdk: fix parallel param

--- namso-01/openjdk/actions.py
+++ namso-01/openjdk/actions.py
@@ -15,25 +15,26 @@ shelltools.export("HOTSPOT_BUILD_JOBS", get.makeJOBS())
 shelltools.export("LC_ALL", "C")
 
 def setup():
-    shelltools.system("./configure \
-                       --disable-tests \
-                       --disable-Werror \
-                       --disable-downloading \
-                       --with-parallel-jobs=-j \
-                       --with-jdk-home=/opt/sun-jdk \
-                       --enable-pulse-java \
-                       --enable-nss \
-                       --with-rhino \
-                       --disable-bootstrap \
-                       --with-jdk-src-zip=7958751eb9ef.tar.gz \
-                       --with-jaxp-src-zip=8f220f7b51c7.tar.gz \
-                       --with-corba-src-zip=8ed5df839fbc.tar.gz \
-                       --with-jaxws-src-zip=652eb396f959.tar.gz  \
-                       --with-openjdk-src-zip=e2f5917da3c1.tar.gz \
-                       --with-hotspot-src-zip=b59e02d9e72b.tar.gz  \
-                       --with-langtools-src-zip=3c8eb52a32ea.tar.gz \
-                       --with-abs-install-dir=/usr/lib/jvm/java-7-openjdk \
-                       --with-pkgversion='PisiLinux build 7.u45_2.4.3-1' ")
+    autotools.rawConfigure("\
+                            --disable-tests \
+                            --disable-Werror \
+                            --disable-downloading \
+                            --with-parallel-jobs=%s \
+                            --with-jdk-home=/opt/sun-jdk \
+                            --enable-pulse-java \
+                            --enable-nss \
+                            --with-rhino \
+                            --disable-bootstrap \
+                            --with-jdk-src-zip=7958751eb9ef.tar.gz \
+                            --with-jaxp-src-zip=8f220f7b51c7.tar.gz \
+                            --with-corba-src-zip=8ed5df839fbc.tar.gz \
+                            --with-jaxws-src-zip=652eb396f959.tar.gz  \
+                            --with-openjdk-src-zip=e2f5917da3c1.tar.gz \
+                            --with-hotspot-src-zip=b59e02d9e72b.tar.gz  \
+                            --with-langtools-src-zip=3c8eb52a32ea.tar.gz \
+                            --with-abs-install-dir=/usr/lib/jvm/java-7-openjdk \
+                            --with-pkgversion='PisiLinux build 7.u45_2.4.3-1' \
+                           " % get.makeJOBS())
 
 def build():
     autotools.make()
