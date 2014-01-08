8f9b26bc171e5f7d3f1e3ce869351eaa4d71eaa8
alihanozturk <alihan.ozturk@fenerbahce.com.tr>
2014-01-08 22:49:03 +0200

    delete

--- alihanozturk/makehuman/actions.py
+++ alihanozturk/makehuman/actions.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 def build():
-    shelltools.system("-f Makefile.Linux")
-    cmaketools.make()
+    autotools.make()
 
 def install():
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
