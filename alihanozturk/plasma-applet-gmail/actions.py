528151db30235e72286c16a50bc0604297a15a89
alihanozturk <alihan.ozturk@fenerbahce.com.tr>
2014-01-13 16:48:48 +0200

    actions kde4

--- alihanozturk/plasma-applet-gmail/actions.py
+++ alihanozturk/plasma-applet-gmail/actions.py
@@ -4,17 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import kde4
 from pisi.actionsapi import get
 
 
 def setup():
-    cmaketools.configure("DQT_QMAKE_EXECUTABLE=qmake-qt4 \
+    kde4.configure("DQT_QMAKE_EXECUTABLE=qmake-qt4 \
                           DCMAKE_BUILD_TYPE=Release \
                           DCMAKE_INSTALL_PREFIX=`kde4-config --prefix`")
 
 def build():
-    cmaketools.make()
+    kde4.make()
 
 def install():
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    kde4.install("DESTDIR=%s" % get.installDIR())
