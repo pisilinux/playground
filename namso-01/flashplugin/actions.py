e04766ce19bbb2acdf4a23742c38c1f5dc606af1
namso-01 <namso-01@hotmail.it>
2014-01-21 20:26:33 +0200

    flashplugin

--- namso-01/flashplugin/actions.py
+++ namso-01/flashplugin/actions.py
@@ -12,18 +12,11 @@ WorkDir = get.ARCH()
 NoStrip = "/"
 
 def install():
-    shelltools.copy("*", "flashplugin-11.2.202.332-x86_64/usr/")
-    distdir = "%s-%s-%s" % (get.srcNAME(), get.srcVERSION(), ARCH)
-
-    shelltools.copytree("%s/usr" % distdir, "%s/usr" % get.installDIR())
+    pisitools.insinto("/usr/bin/", "usr/bin/flash-player-properties")
+    pisitools.insinto("/usr/lib/kde4/", "usr/lib/kde4/kcm_adobe_flash_player.so")
+    pisitools.insinto("/usr/", "usr/share/")
 
     if get.ARCH() == "x86_64":
-        pisitools.remove("/usr/lib/kde4/kcm_adobe_flash_player.so")
-        pisitools.insinto("/usr/lib/kde4", "%s/%s/%s/usr/lib64/kde4/kcm_adobe_flash_player.so" % (get.workDIR(), get.ARCH(), distdir))
-        pisitools.removeDir("/usr/lib64")
+        pisitools.insinto("/usr/lib/kde4", "usr/lib64/kde4/kcm_adobe_flash_player.so")
 
-    pisitools.doexe("%s/usr/libflashplayer.so" % distdir, "/usr/lib/browser-plugins")
-    
-    pisitools.remove("/usr/readme.txt")
-    pisitools.remove("/usr/libflashplayer.so")
-
+    pisitools.doexe("libflashplayer.so", "/usr/lib/browser-plugins")
