156ef7fc20f1fe3cb111e5bdfa145a85ca1f1d91
namso-01 <namso-@hotmail.it>
2014-02-10 22:57:06 +0200

    mariadb

--- namso-01/mariadb/actions.py
+++ namso-01/mariadb/actions.py
@@ -14,41 +14,42 @@ shelltools.export("CXXFLAGS", "%s -fno-strict-aliasing -DBIG_JOINS=1 -felide-con
 shelltools.export("CFLAGS", "%s -fno-strict-aliasing -DBIG_JOINS=1 -fomit-frame-pointer" % get.CFLAGS())
 
 def setup():
+    pisitools.dosed("storage/tokudb/ft-index/ft/ft-ops.cc", "LEAFENTRY leaf_entry;", "LEAFENTRY leaf_entry = 0;")
     cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DSYSCONFDIR=/etc/mysql \
-    -DMYSQL_DATADIR=/var/lib/mysql \
-    -DMYSQL_UNIX_ADDR=/run/mysqld/mysqld.sock \
-    -DDEFAULT_CHARSET=utf8 \
-    -DDEFAULT_COLLATION=utf8_general_ci \
-    -DENABLED_LOCAL_INFILE=ON \
-    -DINSTALL_INFODIR=share/mysql/docs \
-    -DINSTALL_MANDIR=share/man \
-    -DINSTALL_PLUGINDIR=lib/mysql/plugin \
-    -DINSTALL_SCRIPTDIR=bin \
-    -DINSTALL_INCLUDEDIR=include/mysql \
-    -DINSTALL_DOCREADMEDIR=share/mysql \
-    -DINSTALL_SUPPORTFILESDIR=share/mysql \
-    -DINSTALL_MYSQLSHAREDIR=share/mysql \
-    -DINSTALL_DOCDIR=share/mysql/docs \
-    -DINSTALL_SHAREDIR=share/mysql \
-    -DWITH_READLINE=ON \
-    -DWITH_ZLIB=system \
-    -DWITH_SSL=system \
-    -DWITH_LIBWRAP=OFF \
-    -DWITH_EXTRA_CHARSETS=complex \
-    -DWITH_EMBEDDED_SERVER=ON \
-    -DWITH_ARCHIVE_STORAGE_ENGINE=1 \
-    -DWITH_BLACKHOLE_STORAGE_ENGINE=1 \
-    -DWITH_INNOBASE_STORAGE_ENGINE=1 \
-    -DWITH_PARTITION_STORAGE_ENGINE=1 \
-    -DWITH_TOKUDB_STORAGE_ENGINE=1 \
-    -DWITHOUT_EXAMPLE_STORAGE_ENGINE=1 \
-    -DWITHOUT_FEDERATED_STORAGE_ENGINE=1 \
-    -DWITHOUT_PBXT_STORAGE_ENGINE=1")
+                          -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DSYSCONFDIR=/etc/mysql \
+                          -DMYSQL_DATADIR=/var/lib/mysql \
+                          -DMYSQL_UNIX_ADDR=/run/mysqld/mysqld.sock \
+                          -DDEFAULT_CHARSET=utf8 \
+                          -DDEFAULT_COLLATION=utf8_general_ci \
+                          -DENABLED_LOCAL_INFILE=ON \
+                          -DINSTALL_INFODIR=share/mysql/docs \
+                          -DINSTALL_MANDIR=share/man \
+                          -DINSTALL_PLUGINDIR=lib/mysql/plugin \
+                          -DINSTALL_SCRIPTDIR=bin \
+                          -DINSTALL_INCLUDEDIR=include/mysql \
+                          -DINSTALL_DOCREADMEDIR=share/mysql \
+                          -DINSTALL_SUPPORTFILESDIR=share/mysql \
+                          -DINSTALL_MYSQLSHAREDIR=share/mysql \
+                          -DINSTALL_DOCDIR=share/mysql/docs \
+                          -DINSTALL_SHAREDIR=share/mysql \
+                          -DWITH_READLINE=ON \
+                          -DWITH_ZLIB=system \
+                          -DWITH_SSL=system \
+                          -DWITH_LIBWRAP=OFF \
+                          -DWITH_EXTRA_CHARSETS=complex \
+                          -DWITH_EMBEDDED_SERVER=ON \
+                          -DWITH_ARCHIVE_STORAGE_ENGINE=1 \
+                          -DWITH_BLACKHOLE_STORAGE_ENGINE=1 \
+                          -DWITH_INNOBASE_STORAGE_ENGINE=1 \
+                          -DWITH_PARTITION_STORAGE_ENGINE=1 \
+                          -DWITH_TOKUDB_STORAGE_ENGINE=1 \
+                          -DWITHOUT_EXAMPLE_STORAGE_ENGINE=1 \
+                          -DWITHOUT_FEDERATED_STORAGE_ENGINE=1 \
+                          -DWITHOUT_PBXT_STORAGE_ENGINE=1")
 
 def build():
-    cmaketools.make("-j1")
+    cmaketools.make()
 
 def install():
     cmaketools.install("DESTDIR=%s benchdir_root=\"/usr/share/mysql\"" % get.installDIR())
