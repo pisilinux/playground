0f59cddf22bc89d5462cab1a679e40d4f91e4041
Ertuğrul <ertugrulerata@gmail.com>
2014-02-15 23:49:47 +0200

    add url and COYING

--- ertugrul/imagewriter/actions.py
+++ ertugrul/imagewriter/actions.py
@@ -20,4 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    #pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
+    pisitools.dodoc("COPYING")
