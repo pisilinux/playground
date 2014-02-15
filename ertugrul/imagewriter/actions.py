5162366c189138e63d3d8f327cdbc7fdf43cfdaa
Ertuğrul <ertugrulerata@gmail.com>
2014-02-15 23:29:11 +0200

    imagewriter first commit

---
+++ ertugrul/imagewriter/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("qmake DEFINES=USEUDISKS imagewriter.pro \
+                       PREFIX=%s/usr" % get.installDIR())
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    #pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
