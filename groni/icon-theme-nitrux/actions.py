892292c53901e66c644f7bb9027953a9cf490ed3
groni <groni3427@gmail.com>
2014-02-16 15:34:24 +0100

    icon-theme-nitrux change url

--- groni/icon-theme-nitrux/actions.py
+++ groni/icon-theme-nitrux/actions.py
@@ -4,6 +4,8 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+WorkDir = "./"
+
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
