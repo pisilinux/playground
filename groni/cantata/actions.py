e1a3d4c752392084fddabb2f236ac920973adc53
aydemir <yusuf.aydemir@istanbul.com>
2014-02-22 13:29:35 +0200

    cantata svn version

--- groni/cantata/actions.py
+++ groni/cantata/actions.py
@@ -3,11 +3,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import kde4
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import kde4
 
 def setup():
-    kde4.configure()
+    shelltools.system("svn up")
+    kde4.configure("-DENABLE_KDE=OFF")
 
 def build():
     kde4.make()
