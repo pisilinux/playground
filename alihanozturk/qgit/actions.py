0d86b39a26231587d6f070e246650ea6986b2405
alihanozturk <alihan@pisilinux.org>
2014-04-05 22:37:07 +0300

    qgit:version bump

---
+++ alihanozturk/qgit/actions.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt4
+
+def setup():
+    qt4.configure()
+
+def build():
+    qt4.make()
+
+def install():
+    qt4.install()
+
+    pisitools.insinto("/usr/share/pixmaps", "src/resources/qgit.png")
+
+    pisitools.dodoc("README")
