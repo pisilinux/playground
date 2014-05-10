7b39a9b5454c081000c441fd98978a92b6639f5b
suvari <suvarice@gmail.com>
2014-05-10 13:09:49 +0300

    calligra: added kalanlar file, need check

--- suvari/calligra/actions.py
+++ suvari/calligra/actions.py
@@ -4,25 +4,19 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-#from pisi.actionsapi import shelltools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde4
 from pisi.actionsapi import get
-import os
-
-#shelltools.export("HOME", get.workDIR())
 
 def setup():
-    kde4.configure("-DIHAVEPATCHEDQT=1 \
-		    -DCMAKE_SKIP_RPATH=ON \
-		    -DCMAKE_BUILD_TYPE=Release \
-		    -DBUILD_active=OFF \
-		    -DBUILD_cstester:BOOL=OFF \
-		    -DKDE4_BUILD_TESTS=OFF \
-		    -DKDE4_BUILD_TESTS=OFF \
-		    -DBUILD_mobile=OFF \
-		    -DCMAKE_INSTALL_PREFIX=/usr \
-		    -Wno-dev")
+    kde4.configure("-DCMAKE_BUILD_TYPE=Release \
+                    -DCMAKE_SKIP_RPATH=ON \
+                    -DKDE4_BUILD_TESTS=OFF \
+                    -DCMAKE_INSTALL_PREFIX=/usr \
+                    -DCMAKE_INSTALL_LIBDIR=lib \
+                    -DBUILD_active=OFF \
+                    -DWITH_Soprano=OFF")
 
 def build():
     kde4.make()
