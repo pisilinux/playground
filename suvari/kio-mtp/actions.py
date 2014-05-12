ddfb5f6336aa1050fb3f0b5e36d0b4293522d397
namso-01 <namso-01@hotmail.it>
2014-05-12 19:58:47 +0300

    Update actions.py

--- suvari/kio-mtp/actions.py
+++ suvari/kio-mtp/actions.py
@@ -4,11 +4,8 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import get
-from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
-
-# if pisi can't find source directory, see /var/pisi/kio-mtp/work/ and:
-# WorkDir="kio-mtp-"+ get.srcVERSION() +"/sub_project_dir/"
+from pisi.actionsapi import cmaketools
 
 def setup():
     cmaketools.configure("-DCMAKE_BUILD_TYPE=release", installPrefix="/usr")
@@ -19,19 +16,4 @@ def build():
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-# Take a look at the source folder for these file as documentation.
     pisitools.dodoc("COPYING", "README")
-
-# If there is no install rule for a runnable binary, you can 
-# install it to binary directory.
-#    pisitools.dobin("kio-mtp")
-
-# You can use these as variables, they will replace GUI values before build.
-# Package Name : kio-mtp
-# Version : 0.75
-# Summary : Provides KIO Access to MTP devices
-
-# For more information, you can look at the Actions API
-# from the Help menu and toolbar.
-
-# By PiSiDo 2.0.0
