628ebac9cfdcad4c00ee00873ce29400a2e71d21
groni <groni3427@gmail.com>
2014-05-27 17:17:22 +0200

    omnitux Educational activities based on multimedia elements (images, sounds, and text)

---
+++ groni/omnitux/actions.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+
+WorkDir="omnitux-light"
+
+KeepSpecial=["python"]  # do not remove .pyc files
+
+def install():
+    pisitools.insinto("/usr/share/omnitux","*")   
