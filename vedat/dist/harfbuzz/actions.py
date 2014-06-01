cb340d628d1d9802112f854179c5aa94779392df
vedat <vedat@pisi_linux1.0>
2014-06-01 18:19:09 +0300

    harfbuzz:version bump

---
+++ vedat/dist/harfbuzz/actions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    options = "\
+                --prefix=/usr \
+                --libdir=/usr/lib \
+                --includedir=/usr/include \
+                --with-cairo \
+                --with-graphite2=yes \
+                --disable-static \
+                --disable-silent-rules \
+	      "
+    if get.buildTYPE() == "emul32":
+        options += "\
+                    --with-graphite2=no \
+                    --without-cairo \
+                   "
+    
+    #           --with-graphite2 \--with-cairo \
+    #                --with-graphite2=no \
+    #                --without-cairo \
+                   
+    autotools.configure(options)
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
