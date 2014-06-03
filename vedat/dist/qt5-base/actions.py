6388439a229bc68e8481660bd4ca72db985b22ff
vedat <vedat@pisi_linux1.0>
2014-06-03 14:20:04 +0300

    qt5-base:fix

--- vedat/dist/qt5-base/actions.py
+++ vedat/dist/qt5-base/actions.py
@@ -41,20 +41,22 @@ def setup():
     if not get.buildTYPE() == "emul32":
         #-no-pch makes build ccache-friendly
         options = "-v \
-                   -no-pch \
+                   -prefix /usr \
+                   -libdir /usr/lib \
+                   -bindir /usr/bin \
+                   -headerdir /usr/include \
+                   -plugindir /usr/lib/qt5/plugins \
+                   -importdir /usr/lib/qt5/imports \
+                   -datadir /usr/share/qt5 \
+                   -docdir /usr/share/doc/qt5 \
+                   -libexecdir /usr/libexec \
+                   -translationdir /usr/share/qt5/translations \
+                   -sysconfdir /etc \
+                   -archdatadir /usr/lib/qt5 \
                    -confirm-license \
                    -opensource \
                    -optimized-qmake \
                    -nomake tests \
-                   -no-rpath \
-                   -no-mtdev \
-                   -no-sql-db2 \
-                   -no-sql-oci \
-                   -no-sql-odbc \
-                   -no-sql-psql \
-                   -no-sql-sqlite \
-                   -no-sql-sqlite2 \
-                   -no-sql-tds \
                    -release \
                    -shared \
                    -accessibility \
@@ -75,10 +77,22 @@ def setup():
                    -plugin-sql-psql \
                    -no-sql-ibase \
                    -no-sql-tds \
+                   -no-rpath \
+                   -no-mtdev \
+                   -no-sql-db2 \
+                   -no-sql-oci \
+                   -no-pch \
                    -I/usr/include/firebird/ \
                    -I/usr/include/postgresql/server/ \
                    -no-separate-debug-info \
                    -no-strip " 
+                   
+                   #-no-sql-odbc \
+                   #-no-sql-psql \
+                   #-no-sql-sqlite \
+                   #-no-sql-sqlite2 \
+                   
+                   
     else:
         pisitools.dosed("qtbase/mkspecs/linux-g++-64/qmake.conf", "-m64", "-m32")
         shelltools.export("LDFLAGS", "-m32 %s" % get.LDFLAGS())
@@ -126,39 +140,7 @@ def build():
     autotools.make()
 
 def install():
-    if get.buildTYPE() == "emul32":
-        qt5.install("INSTALL_ROOT=%s32" % get.installDIR())
-        shelltools.move("%s32/usr/lib32" % get.installDIR(), "%s/usr" % get.installDIR())
-        return
-    
-    pisitools.dodir(qt5.bindir)
-    pisitools.dodir(qt5.libdir)
-    qt5.install("INSTALL_ROOT=%s" % get.installDIR())
-    
-    #I hope qtchooser will manage this issue
-    #for bin in shelltools.ls("%s/usr/lib/qt5/bin" % get.installDIR()):
-        #pisitools.dosym("/usr/lib/qt5/bin/%s" % bin, "/usr/bin/%s-qt5" % bin)
-    
-    # We should work on Turkish translations :)
-    shelltools.export("LD_LIBRARY_PATH", "%s%s" % (get.installDIR(), qt5.libdir))
-    shelltools.system("%s%s/lrelease l10n-tr/*.ts" % (get.installDIR(), bindirQt5))
-    pisitools.insinto(qt5.translationdir, "l10n-tr/*.qm")
-
-    # Fix all occurances of WorkDir in pc files
-    pisitools.dosed("%s%s/pkgconfig/*.pc" % (get.installDIR(), qt5.libdir), "%s/qt-x11-opensource-src-%s" % (get.workDIR(), get.srcVERSION()), qt5.prefix)
-
-    mkspecPath = "%s/mkspecs" %  qt5.archdatadir
-
-    for root, dirs, files in os.walk("%s%s" % (get.installDIR(),  qt5.archdatadir)):
-        # Remove unnecessary spec files..
-        if root.endswith(mkspecPath):
-            for dir in dirs:
-                if not dir.startswith("linux") and dir not in ["common","qws","features","default"]:
-                    pisitools.removeDir(os.path.join(mkspecPath,dir))
-        for name in files:
-            if name.endswith(".prl"):
-                pisitools.dosed(os.path.join(root, name), "^QMAKE_PRL_BUILD_DIR.*", "")
-
+    autotools.install()
     pisitools.dodoc("LGPL_EXCEPTION.txt", "LICENSE.*")
 
 
