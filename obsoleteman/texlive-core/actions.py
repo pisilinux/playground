4a8361a9f5e5fea55fd427af28d516c92b0eb9d8
marcin- <marcin.bojara@gmail.com>
2014-06-11 21:04:11 +0200

    update build files for texlive-core

--- obsoleteman/texlive-core/actions.py
+++ obsoleteman/texlive-core/actions.py
@@ -33,17 +33,157 @@ def install():
     for i in shelltools.ls("."):
         if shelltools.isDirectory(i) and not i.startswith("texmf"):
             shelltools.copytree(i, "%s/usr/share/texmf-dist/" % get.installDIR())
-    --end--
 
-    ### copy config file to texmf-config ??
-    pisitools.dodir("/etc/texmf/tex/context/config")
-    shelltools.copy("%s/usr/share/texmf-dist/tex/context/config/cont-usr.tex" % get.installDIR(), \
-                     "%s/etc/texmf/tex/context/config/cont-usr.tex" % get.installDIR())
+    # clean config files
+    pisitools.dosed("tex/generic/config", "DO NOT EDIT", deleteLine=True, filePattern="language\.d..$")
+    pisitools.dosed("texmf-dist/web2c/updmap.cfg", "^(#!\s*)?(Map|MixedMap)", deleteLine=True)
+    pisitools.dosed("%s/usr/share/texmf-dist/web2c/fmtutil.cnf" % get.installDIR(), "aleph", deleteLine=True)
 
- ## ??
-    ## old packages, we will not provide them
-    #pisitools.remove("/usr/share/texmf-dist/tex/plain/config/omega.ini")
-    #pisitools.remove("/usr/share/texmf-dist/tex/plain/config/aleph.ini")
-    #pisitools.removeDir("/usr/share/texmf-dist/scripts/context/stubs/mswin/")
+    # install config files
+    cfs = ["chktex/chktexrc",
+           "web2c/mktex.cnf",
+           "web2c/updmap.cfg",
+           "web2c/fmtutil.cnf",
+           "dvips/config/config.ps",
+           "dvipdfmx/dvipdfmx.cfg",
+           "tex/generic/config/language.dat",
+           "tex/generic/config/language.def",
+           "tex/generic/config/pdftexconfig.tex",
+           "ttf2pk/ttf2pk.cfg",
+           "xdvi/XDvi"]
+    for cf in cfs:
+        d = "/".join(cf.split("/")[:-1])
+        p = cf if shelltools.isFile(cf) else "texmf-dist/%s" % cf
+        pisitools.insinto("/etc/texmf/%s" % d, p)
+    #pisitools.dosym("/etc/texmf/web2c/texmf.cnf", "/usr/share/texmf-dist/web2c/texmf.cnf")
 
+    # fix sandbox violations
+#    pisitools.dosed("texmf-dist/scripts/texlive/texlinks.sh", "src=\$2", r'src="%s/$2"' % get.installDIR())
+    pisitools.dosed("texmf-dist/scripts/texlive/texlinks.sh", '"\$symlinkdir', r'"%s/$ymlinkdir' % get.installDIR())
+
+    # create symlinks
+    pisitools.dodir("/usr/bin")
+    shelltools.system("texmf-dist/scripts/texlive/texlinks.sh -f %s/usr/share/texmf-dist/web2c/fmtutil.cnf %s/usr/bin" % ((get.installDIR(), ) * 2))
+
+    # remove upstream updmap.cfg: it contains too many maps
+    pisitools.remove("/usr/share/texmf-dist/web2c/updmap.cfg")
+
+    # manpages already in texlive-bin
     pisitools.removeDir("/usr/share/texmf-dist/doc/man")
+
+    # remove unneeded dir
+    pisitools.removeDir("/usr/share/texmf-dist/scripts/context/stubs/mswin")
+
+    # link programs from /usr/share/texmf-dist/scripts
+    linked_scripts="""
+a2ping/a2ping.pl
+accfonts/mkt1font
+accfonts/vpl2ovp
+accfonts/vpl2vpl
+adhocfilelist/adhocfilelist.sh
+arara/arara.sh
+bundledoc/arlatex
+bundledoc/bundledoc
+checkcites/checkcites.lua
+chktex/chkweb.sh
+chktex/deweb.pl
+context/perl/mptopdf.pl
+context/stubs/unix/context
+context/stubs/unix/ctxtools
+context/stubs/unix/luatools
+context/stubs/unix/mtxrun
+context/stubs/unix/pstopdf
+context/stubs/unix/texexec
+context/stubs/unix/texmfstart
+ctanify/ctanify
+ctanupload/ctanupload.pl
+de-macro/de-macro
+dosepsbin/dosepsbin.pl
+dtxgen/dtxgen
+dviasm/dviasm.py
+epstopdf/epstopdf.pl
+findhyph/findhyph
+fontools/afm2afm
+fontools/autoinst
+fontools/ot2kpx
+fragmaster/fragmaster.pl
+installfont/installfont-tl
+latex2man/latex2man
+latexdiff/latexdiff-vc.pl
+latexdiff/latexdiff.pl
+latexdiff/latexrevise.pl
+latexfileversion/latexfileversion
+latexmk/latexmk.pl
+latexpand/latexpand
+ltxfileinfo/ltxfileinfo
+lua2dox/lua2dox_filter
+luaotfload/luaotfload-tool.lua
+match_parens/match_parens
+mf2pt1/mf2pt1.pl
+mkjobtexmf/mkjobtexmf.pl
+oberdiek/pdfatfi.pl
+pdfcrop/pdfcrop.pl
+pdfjam/pdf180
+pdfjam/pdf270
+pdfjam/pdf90
+pdfjam/pdfbook
+pdfjam/pdfflip
+pdfjam/pdfjam
+pdfjam/pdfjam-pocketmod
+pdfjam/pdfjam-slides3up
+pdfjam/pdfjam-slides6up
+pdfjam/pdfjoin
+pdfjam/pdfnup
+pdfjam/pdfpun
+pfarrei/a5toa4.tlu
+pfarrei/pfarrei.tlu
+pkfix-helper/pkfix-helper
+pkfix/pkfix.pl
+ps2eps/ps2eps.pl
+purifyeps/purifyeps
+simpdftex/simpdftex
+sty2dtx/sty2dtx.pl
+texcount/texcount.pl
+texdef/texdef.pl
+texdiff/texdiff
+texdirflatten/texdirflatten
+texdoc/texdoc.tlu
+texdoctk/texdoctk.pl
+texlive/allcm.sh
+texlive/allneeded.sh
+texlive/dvi2fax.sh
+texlive/dvired.sh
+texlive/e2pall.sh
+texlive/fmtutil-sys.sh
+texlive/fmtutil.sh
+texlive/fontinst.sh
+texlive/kpsetool.sh
+texlive/kpsewhere.sh
+texlive/ps2frag.sh
+texlive/pslatex.sh
+texlive/rungs.tlu
+texlive/texconfig-dialog.sh
+texlive/texconfig-sys.sh
+texlive/texconfig.sh
+texlive/texlinks.sh
+texlive/updmap-sys.sh
+texlive/updmap.pl
+texliveonfly/texliveonfly.py
+texloganalyser/texloganalyser
+thumbpdf/thumbpdf.pl
+typeoutfileinfo/typeoutfileinfo.sh
+xindy/texindy.pl
+xindy/xindy.pl
+"""
+    for p in linked_scripts.split():
+        bn = shelltools.baseName(p).split(".")[0]
+        if shelltools.isFile("%s/usr/share/texmf-dist/scripts/%s" % (get.installDIR(), p)):
+            pisitools.dosym("/usr/share/texmf-dist/scripts/%s" % p, "/usr/bin/%s" % bn)
+    pisitools.dosym("/usr/share/texmf-dist/scripts/listings-ext/listings-ext.sh", "/usr/bin/listings-ext.sh")
+    pisitools.dosym("allcm", "/usr/bin/allec")
+    pisitools.dosym("fmtutil", "/usr/bin/mktexfmt")
+    pisitools.dosym("kpsetool", "/usr/bin/kpsexpand")
+    pisitools.dosym("kpsetool", "/usr/bin/kpsepath")
+    pisitools.dosym("epstopdf", "/usr/bin/repstopdf")
+    pisitools.dosym("pdfcrop", "/usr/bin/rpdfcrop")
+    pisitools.dosym("luaotfload-tool", "/usr/bin/mkluatexfontdb")
