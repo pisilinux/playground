efeb3f833b3addbfbc25763b9266261611db9e33
alihanozturk <alihan@pisilinux.org>
2014-06-13 17:34:09 +0300

    metacity:add patch

--- alihanozturk/metacity/actions.py
+++ alihanozturk/metacity/actions.py
@@ -9,6 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-if")
     autotools.configure("--disable-static \
                          --disable-schemas-compile")
     
