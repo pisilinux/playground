76f6dbadc9c5df6669a51e366ee258cc06777e08
tribunal2000 <tribunal2000@gmail.com>
2014-07-03 23:25:09 +0400

    fife:build fix

--- tribunal2000/fife/actions.py
+++ tribunal2000/fife/actions.py
@@ -10,14 +10,15 @@ from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 def build():
-    scons.make("-j1 install-shared install-python install-dev \
+    scons.make("-j1 fife-shared fife-python \
                 --lib-dir=/usr/lib \
-                --prefix=/usr \
-                --python-prefix=/usr/lib/python2.7/site-packages/")
-
+	        --prefix=/usr \
+	        --python-prefix=/usr/lib/python2.7/site-packages/ ")
+	        
 def install():
-    scons.install()
-
-    pisitools.dohtml("doc/*")
-    pisitools.dodoc("COPYRIGHT", "README*")
+    scons.install("-j1 install-shared install-python install-dev \
+	           --lib-dir=/usr/lib \
+	           --prefix=/usr \
+	           --python-prefix=/usr/lib/python2.7/site-packages/ \
+	           --install-sandbox=%s" % get.installDIR())
 
