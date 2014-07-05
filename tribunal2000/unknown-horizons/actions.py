cdaecdd99f79b3606e5344bf2a27d0de6bc405a9
tribunal2000 <tribunal2000@gmail.com>
2014-07-05 22:18:06 +0400

    Unknown Horizones:fix build

--- tribunal2000/unknown-horizons/actions.py
+++ tribunal2000/unknown-horizons/actions.py
@@ -4,11 +4,14 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools 
 
 def build():
+    pythonmodules.compile ()
     
 def install():
+    pythonmodules.install ()
+    pythonmodules.fixCompiledPy("/usr/share/unknown-horizons")
     
+    pisitools.dodoc ("README")
