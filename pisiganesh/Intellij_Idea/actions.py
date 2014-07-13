a6bd11771f099efda492fb7cd5400ec7261374c7
Ganesh Iyer <iyer.gb.linux@gmail.com>
2014-07-13 12:48:36 +0530

    IntelliJ Idea Version Update to 13.1.3

--- pisiganesh/Intellij_Idea/actions.py
+++ pisiganesh/Intellij_Idea/actions.py
@@ -8,11 +8,13 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "idea-IC-133.193"
+#WorkDir = "idea-IC-133.193"
+WorkDir = "idea-IC-135.909"
 
 def install():
     shelltools.cd("..")
     pisitools.dodir("/opt")
-    pisitools.insinto("/opt","idea-IC-133.193")
+#   pisitools.insinto("/opt","idea-IC-133.193")
+    pisitools.insinto("/opt","idea-IC-135.909")
     
 # By PiSiDo 2.0.0
