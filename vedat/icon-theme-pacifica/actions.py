4715831eb9be64d9c12418332b431f11d10dc026
aydemir <yusuf.aydemir@istanbul.com>
2014-07-28 20:31:23 +0300

    pacifica

--- vedat/icon-theme-pacifica/actions.py
+++ vedat/icon-theme-pacifica/actions.py
@@ -5,10 +5,14 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 WorkDir = "./"
 
 def install():
     pisitools.insinto("/usr/share/icons", "pacifica-icon-theme-master/Pacifica")
+    shelltools.chmod("%s/usr/share/icons/Pacifica/index.theme" % get.installDIR(), 0644)
+    shelltools.chmod("%s/usr/share/icons/Pacifica/CREDITS" % get.installDIR(), 0644)
     
 #    pisitools.dodoc("AUTHORS", "LICENSE")
