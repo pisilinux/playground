44dffa4c94954c967353ddd0a8eebc2e8ab8dbbb
mathias-freire <mathiasfreire45@gmail.com>
2014-09-09 02:26:27 +0300

    Mesa 1.3.0-rc3, Added OpenCL and XvMC support

---
+++ narcisse/mesa/actions.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+Libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib"
+
+
+def setup():
+    autotools.autoreconf("-vif")
+
+    options ="\
+              --with-dri-driverdir=/usr/lib/xorg/modules/dri \
+              --with-gallium-drivers=r300,r600,nouveau,svga,swrast \
+              --with-dri-drivers=i915,i965,r200,radeon,nouveau,swrast \
+              --with-egl-platforms=x11,drm,wayland \
+              --enable-xa \
+              --enable-dri \
+              --enable-egl \
+              --enable-gbm \
+              --enable-glx \
+              --enable-dri3 \
+              --enable-gles1 \
+              --enable-gles2 \
+              --enable-vdpau \
+              --enable-openvg \
+              --enable-osmesa \
+              --enable-glx-tls \
+              --enable-gallium-egl \
+              --enable-gallium-gbm \
+              --enable-gallium-llvm \
+              --enable-shared-glapi \
+              --enable-texture-float \
+              --enable-xvmc \
+             "
+
+    if get.buildTYPE() == "emul32":
+        # compile with llvm doesn't work for now, test it later
+        
+             
+        options += " --with-dri-driverdir=/usr/lib32/xorg/modules/dri \
+                     --with-gallium-drivers=r600,nouveau,swrast \
+                     --with-clang-libdir=/usr/lib32 \
+                     --disable-asm \
+                     --disable-gallium-llvm \
+                     --enable-32-bit"
+                     
+    elif get.ARCH() == "x86_64":
+       
+        options += " --with-clang-libdir=/usr/lib \
+                     --enable-opencl \
+                     --enable-opencl-icd \
+                   "
+
+    autotools.configure(options)
+    pisitools.dosed("libtool","( -shared )", " -Wl,--as-needed\\1")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())    
+
+    pisitools.domove("%s/libGL.so.1.2.0" % Libdir, "%s/mesa" % Libdir)    
+    pisitools.dosym("libGL.so.1.2.0", "%s/libGL.so.1.2" % Libdir)
+    
+
+
+    if get.buildTYPE() == "emul32":
+        return
+
+    pisitools.dodoc("docs/COPYING")
+    pisitools.dohtml("docs/*")
