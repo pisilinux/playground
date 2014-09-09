b3299aa15ff533c72c859eb7296e437403f1b130
obsoleteman <tulliana@gmail.com>
2014-09-09 13:42:23 +0300

    fix re2c and ninja for groni

--- groni/ninja/actions.py
+++ groni/ninja/actions.py
@@ -6,27 +6,25 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-#WorkDIR = "ninja-1.5.1" % get.srcVERSION()
 
 def build():
-    pythonmodules.configure("./bootstrap.py \
-                          --emacs -Q --batch -f batch-byte-compile misc/ninja-mode.el \
-                          --asciidoc doc/manual.asciidoc")
+    shelltools.system("python bootstrap.py")
+    shelltools.system("asciidoc doc/manual.asciidoc")
 
-def check():
-    pythonmodules.check("python2 ./configure.py --with-gtest=/usr/src/gtest\
-                        -./ninja ninja_test\
-                        -./ninja_test --gtest_filter=-SubprocessTest.SetWithLots")
+#def check():
+    #needs new package gtest -> ignore it for now
+    #shelltools.system("python ./configure.py --with-gtest=/usr/src/gtest")
+    #shelltools.system("./ninja ninja_test")
+    #shelltools.system("./ninja_test --gtest_filter=-SubprocessTest.SetWithLots")
 
 def install():
-    pythonmodules.install("install -m755 -D ninja $pkdir/usr/bin/ninja"
-                          -"install -m644 -D doc/manual.asciidoc $pkgdir/usr/share/doc/ninja/manual.asciidoc"
-                          -"install -m644 -D doc/manual.html $pkgdir/usr/share/doc/ninja/manual.html"
-                          -" install -m644 -D misc/ninja-mode.el $pkgdir/usr/share/emacs/site-lisp/ninja-mode.el"
-                          -"install -m644 -D misc/ninja-mode.elc $pkgdir/usr/share/emacs/site-lisp/ninja-mode.elc"
-                          -"install -m644 -D misc/bash-completion $pkgdir/usr/share/bash-completion/completions/ninja"
-                          -"install -m644 -D misc/zsh-completion $pkgdir/usr/share/zsh/site-functions/_ninja")
+    pisitools.dobin("ninja", "/usr/bin")
 
-    pisitools.dodoc("HACKING.md", "COPYING", "RELEASING", "README")
+    pisitools.insinto("/usr/share/bash-completion/completions", "misc/bash-completion", "ninja")
+
+    pisitools.dodoc("HACKING.md", "COPYING", "RELEASING", "README", "doc/manual.asciidoc")
+
+    pisitools.dohtml("doc/manual.html")
