04e05ee546645f9bc74b32eb64ac8444376d53c4
vedat <vedat@pisi_linux1.0>
2014-09-13 17:34:00 +0300

    xscreensaver-5.30

--- vedat/dist/xscreensaver/actions.py
+++ vedat/dist/xscreensaver/actions.py
@@ -13,39 +13,43 @@ def setup():
     autotools.configure("--enable-locking \
                          --disable-root-passwd \
                          --with-x-app-defaults=/usr/share/X11/app-defaults \
+                         --with-configdir=/usr/share/xscreensaver/config \
+                         --with-hackdir=/usr/libexec/xscreensaver \
                          --with-dpms-ext \
+                         --with-gtk \
                          --with-xinerama-ext \
                          --with-xf86vmode-ext \
                          --with-xf86gamma-ext \
                          --with-randr-ext \
+                         --with-xdbe-ext \
+                         --with-xinput-ext \
+                         --with-xshm-ext \
                          --with-proc-interrupts \
+                         --with-gl \
+                         --with-xpm \
+                         --with-jpeg \
                          --with-pam \
+                         --with-pixbuf \
+                         --x-libraries=/usr/lib \
+                         --x-includes=/usr/include \
                          --without-kerberos \
                          --without-shadow \
                          --without-passwd-helper \
                          --without-login-manager \
-                         --with-gtk \
                          --without-motif \
-                         --with-gl \
-                         --with-gle \
-                         --without-pixbuf \
-                         --with-xpm \
-                         --with-jpeg \
-                         --with-xshm-ext \
-                         --with-xdbe-ext \
                          --without-readdisplay \
                          --with-browser=xdg-open \
                          --without-setuid-hacks")
 
 def build():
-    autotools.make("-Wunused-but-set-variable")
+    autotools.make()
 
 def install():
+    
     pisitools.dodir("/etc/pam.d")
-
     autotools.rawInstall("install_prefix=%s" % get.installDIR())
-
+    
     # Remove webcollage its pr0n enabled
-    pisitools.remove("/usr/libexec/xscreensaver/webcollage")
+    # pisitools.remove("/usr/libexec/xscreensaver/webcollage")
     pisitools.remove("/usr/share/man/man6/webcollage.*")
     pisitools.remove("/usr/share/xscreensaver/config/webcollage.xml")
