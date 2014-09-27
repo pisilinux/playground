19bc7e47a4617fcabb2dc8a9a96e922969473c73
groni <groni3427@gmail.com>
2014-09-27 13:43:14 +0200

    feh is an X11 image viewer aimed mostly at console users

---
+++ groni/graphics/feh/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+WorkDir = "feh-2.12"
+
+#def setup():
+
+def build():
+    autotools.make("PREFIX=/usr")
+def install():
+    autotools.rawInstall("PREFIX=/usr DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README", "COPYING", "TODO")
