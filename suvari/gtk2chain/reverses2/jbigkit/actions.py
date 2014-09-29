28d2b91e542341cfadd0c402096fe887741a7691
obsoleteman <tulliana@gmail.com>
2014-09-29 21:00:39 +0300

    jbigkit: fix installation

--- suvari/gtk2chain/reverses2/jbigkit/actions.py
+++ suvari/gtk2chain/reverses2/jbigkit/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-#WorkDir="jbigkit"
-
 def build():
     autotools.make("CC=%s CFLAGS='%s'" % (get.CC(), get.CFLAGS()))
 
@@ -17,7 +15,20 @@ def check():
     autotools.make("test")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s prefix=/usr libdir=/usr/lib" % get.installDIR())
+    #install binaries
+    binaries=["jbgtopbm", "pbmtojbg", "jbgtopbm85", "pbmtojbg85"]
+    for mybin in binaries:
+        pisitools.dobin("pbmtools/%s" % mybin)
+
+    #install shared libraries
+    pisitools.dolib_so("libjbig/*.so.*")
+
+    #symlinks
+    pisitools.dosym("libjbig.so.2.1", "/usr/lib/libjbig.so")
+    pisitools.dosym("libjbig85.so.2.1", "/usr/lib/libjbig85.so")
+
+    #install headers
+    pisitools.insinto("/usr/include", "libjbig/*.h")
 
     pisitools.doman("pbmtools/jbgtopbm.1", "pbmtools/pbmtojbg.1")
     pisitools.dodoc("ANNOUNCE", "CHANGES", "COPYING", "TODO")
