8b5fa66aa0d31127b573d495df846c218735f761
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2014-09-30 09:01:38 +0300

    lxde:reload

--- ayhanyalcinsoy/Desktop/lxde/base/libfm/actions.py
+++ ayhanyalcinsoy/Desktop/lxde/base/libfm/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = ""
+WorkDir = "libfm-%s" % (get.srcVERSION())
 
 def setup():
     autotools.configure("--disable-static \
