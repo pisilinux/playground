0b2dd4ebb99120cb196b49fa9cb5df5379ccd3dc
vedat <vedat@pisilinux.org>
2014-10-01 04:43:53 +1100

    revert ibjpeg-turbo to 1.3.1

--- suvari/gtk2chain/reverses/libjpeg-turbo/actions.py
+++ suvari/gtk2chain/reverses/libjpeg-turbo/actions.py
@@ -10,13 +10,10 @@ from pisi.actionsapi import pisitools
 def setup():
     autotools.autoreconf("-fi")
     if not get.buildTYPE() == "emul32":
-        autotools.configure("--with-jpeg8 \
-                             --with-mem-srcdst \
-                             --with-java=/usr/bin/java ")
+        autotools.configure("--with-jpeg8")
     else:
         pisitools.dosed("configure", "(NAFLAGS='-fel)f64( -DELF -D__x86_64__)", "\\1f32\\2")
-        autotools.configure("--with-jpeg8 \
-                             --without-simd")
+        autotools.configure("--with-jpeg8 --without-simd")
 
 def build():
     autotools.make()
