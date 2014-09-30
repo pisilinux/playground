821b39ef9d9e65d2f6c316075240798d0d72a13b
vedat <vedat@pisilinux.org>
2014-10-01 06:29:05 +1100

    gdk-pixbuf-2.31.1: version bump.

--- suvari/gtk2chain/gtk2deps/gdk-pixbuf/actions.py
+++ suvari/gtk2chain/gtk2deps/gdk-pixbuf/actions.py
@@ -14,30 +14,20 @@ def setup():
                  --disable-silent-rules \
                  --with-libjasper \
                  --with-x11 \
+                 --with-libpng \
+                 --with-libtiff \
+                 --with-libjpeg \
+                 --enable-introspection \
                  --with-included-loaders=png \
               "
 
-    options += "\
-                 --bindir=/_emul32/bin \
-                 --disable-introspection \
-               " if get.buildTYPE() == "emul32" else \
-               "\
-                 --enable-introspection \
-               "
     autotools.configure(options)
 
-    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
-    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
-    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    if get.buildTYPE() == "emul32":
-        pisitools.domove("/_emul32/bin/gdk-pixbuf-query-loaders", "/usr/bin", "gdk-pixbuf-query-loaders-32")
-        pisitools.removeDir("/_emul32")
-        return
-    pisitools.dosym("/usr/bin/gdk-pixbuf-query-loaders", "/usr/bin/gdk-pixbuf-query-loaders-64")
+  
     pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
