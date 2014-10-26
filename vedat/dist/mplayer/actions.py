038c22511bbbee3f4401a10fc52a8919df233299
vedat <vedat@pisi_linux1.0>
2014-10-26 21:59:29 +0200

    mplayer vb.

--- vedat/dist/mplayer/actions.py
+++ vedat/dist/mplayer/actions.py
@@ -11,103 +11,33 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pythonmodules
 
 pisitools.flags.sub("-O[\ds]+", "-O3")
-
+WorkDir="mplayer-checkout-2014-10-26"
 def setup():
 #    for f in ["configure", "libmpdemux/demux_rtp.cpp", "libmpdemux/demux_rtp_internal.h"]:
 #        pisitools.dosed(f, "([\"<])(liveMedia|BasicUsageEnvironment)(\.hh)([\">])", "\\1\\2/\\2\\3\\4")
 #    pisitools.dosed("libmpdemux/demux_rtp.cpp", "GroupsockHelper.hh", "groupsock/GroupsockHelper.hh")
-    shelltools.copytree("../ffmpeg-2.2.4", "ffmpeg")
+    shelltools.copytree("../ffmpeg-2.4.2", "ffmpeg")
     autotools.rawConfigure(' \
-                             --confdir=/usr/share/mplayer \
-                             --datadir=/usr/share/mplayer \
-                             --prefix=/usr \
-                             --charset=UTF-8 \
-                             --disable-3dfx \
-                             --disable-altivec \
-                             --disable-arts \
-                             --disable-ass-internal \
-                             --disable-bitmap-font \
-                             --disable-debug \
-                             --disable-dvdread-internal \
-                             --disable-esd \
-                             --enable-fribidi \
-                             --disable-ggi \
-                             --disable-libdvdcss-internal \
-                             --disable-live \
-                             --disable-mga \
-                             --disable-nas \
-                             --disable-rpath \
-                             --disable-svga \
-                             --disable-tdfxfb \
-                             --disable-tdfxvid \
-                             --disable-tga \
-                             --disable-tv-v4l1 \
-                             --enable-aa \
-                             --enable-alsa \
-                             --enable-ass \
-                             --enable-bl \
-                             --enable-bluray \
-                             --enable-caca \
-                             --enable-cmov \
-                             --enable-dvb \
-                             --enable-dvdnav \
-                             --enable-dvdread \
-                             --enable-fbdev \
-                             --enable-ffmpeg_a \
-                             --enable-fontconfig \
-                             --enable-freetype \
-                             --enable-ftp \
-                             --enable-gif \
-                             --enable-gl \
-                             --enable-gui \
-                             --enable-inet6 \
-                             --enable-jack \
-                             --enable-joystick \
-                             --enable-jpeg \
-                             --enable-langinfo \
-                             --enable-liblzo \
-                             --enable-libopencore_amrnb \
-                             --enable-libopencore_amrwb \
-                             --enable-libvorbis \
-                             --enable-lirc \
-                             --enable-mad \
-                             --enable-mencoder \
-                             --enable-menu \
-                             --enable-mmx \
-                             --enable-mmxext \
-                             --enable-networking \
-                             --enable-openal \
-                             --enable-ossaudio \
-                             --enable-png \
-                             --enable-pulse \
-                             --enable-radio \
-                             --enable-radio-capture \
-                             --enable-radio-v4l2 \
-                             --enable-real \
-                             --enable-rtc \
-                             --enable-runtime-cpudetection \
-                             --enable-sdl \
-                             --enable-shm \
-                             --enable-smb \
-                             --enable-sse \
-                             --enable-sse2 \
-                             --enable-theora \
-                             --enable-tv \
-                             --enable-tv-v4l2 \
-                             --enable-unrarexec \
-                             --enable-v4l2 \
-                             --enable-vdpau \
-                             --enable-x11 \
-                             --enable-xf86keysym \
-                             --enable-xinerama \
-                             --enable-xshape \
-                             --enable-xv \
-                             --enable-xvid \
-                             --enable-xvmc \
-                             --extra-ldflags="-lvorbisenc -lvorbis -logg" \
-                             --extra-libs="-lfribidi -lglib-2.0 -lopenal -ljack -lxvidcore -lfontconfig -lass -laa -lX11 -lXext" \
-                             --language=en \
-                             --with-xvmclib=XvMCW \
+                            --prefix=/usr \
+                            --enable-runtime-cpudetection \
+                            --enable-gui \
+                            --disable-arts \
+                            --disable-liblzo \
+                            --disable-speex \
+                            --disable-openal \
+                            --disable-libdv \
+                            --disable-musepack \
+                            --disable-esd \
+                            --disable-mga \
+                            --disable-ass-internal \
+                            --disable-cdparanoia \
+                            --enable-xvmc \
+                            --enable-radio \
+                            --enable-radio-capture \
+                            --enable-smb \
+                            --language=all \
+                            --enable-ffmpeg_a \
+                            --confdir=/etc/mplayer \
                             ')
 
                             # stuff that fail hede=yes check, but working with hede=auto
