8bcb912a3ce6facefd3b3fbf0380898a34132485
vedat <vedat@pisilinux.org>
2014-11-02 14:36:57 +0200

    mesa: version bump

--- vedat/dist/mesa/actions.py
+++ vedat/dist/mesa/actions.py
@@ -12,34 +12,50 @@ Libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib"
 def setup():
     autotools.autoreconf("-vif")
 
+# --enable-sysfs option provides better hardware information support with "lspci"
+# --enable-32-bit option is not present anymore. Although build fails in emul32. With --disable-asm option, not fail. Needs to be tested.
+
     options ="\
               --with-dri-driverdir=/usr/lib/xorg/modules/dri \
-              --with-gallium-drivers=r300,r600,nouveau,svga,swrast \
+              --with-gallium-drivers=r300,r600,radeonsi,nouveau,svga,swrast \
               --with-dri-drivers=i915,i965,r200,radeon,nouveau,swrast \
               --with-egl-platforms=x11,drm,wayland \
-              --enable-gallium-llvm \
-              --enable-egl \
-              --enable-gallium-egl \
-              --enable-shared-glapi \
-              --enable-gbm \
-              --enable-glx-tls \
+              --enable-llvm-shared-libs \
+              --enable-xa \
               --enable-dri \
+              --enable-egl \
+              --enable-gbm \
               --enable-glx \
-              --enable-osmesa \
               --enable-gles1 \
               --enable-gles2 \
-              --enable-texture-float \
-              --enable-xa \
               --enable-vdpau \
-              --enable-dri3 \
+              --enable-openvg \
+              --enable-osmesa \
+              --enable-sysfs \
+              --enable-xvmc \
+              --enable-glx-tls \
+              --enable-gallium-egl \
+              --enable-gallium-gbm \
+              --enable-gallium-llvm \
+              --enable-shared-glapi \
+              --enable-texture-float \
+              --enable-llvm-shared-libs \
+              --with-clang-libdir=/usr/lib \
              "
 
     if get.buildTYPE() == "emul32":
         # compile with llvm doesn't work for now, test it later
         options += " --with-dri-driverdir=/usr/lib32/xorg/modules/dri \
-                     --disable-gallium-llvm \
                      --with-gallium-drivers=r600,nouveau,swrast \
-                     --enable-32-bit"
+                     --with-clang-libdir=/usr/lib32 \
+                     --disable-gallium-llvm \
+                     --disable-asm "
+
+    elif get.ARCH() == "x86_64":
+
+        options += " --with-clang-libdir=/usr/lib \
+                     --enable-opencl-icd \
+                   "
 
     autotools.configure(options)
     pisitools.dosed("libtool","( -shared )", " -Wl,--as-needed\\1")
