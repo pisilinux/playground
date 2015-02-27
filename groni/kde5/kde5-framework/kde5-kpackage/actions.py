1614c8dff4e71b7cfeb91ac1492be989ca3a8963
groni <groni3427@gmail.com>
2015-02-28 00:44:35 +0100

    kde5-kpackage for kde5

---
+++ groni/kde5/kde5-framework/kde5-kpackage/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    #shelltools.cd("kpackage")
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
+                          -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DLIB_INSTALL_DIR=lib \
+                          -DBUILD_TESTING=OFF")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "COPYING.LIB", "README.md")
