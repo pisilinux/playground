53b64c3b45259d8801d6e4839edbb4489aa3bd84
groni <groni3427@gmail.com>
2015-04-05 12:35:21 +0200

    add new sddm working with system-settings

--- groni/kde5/kde5-plasma/kde5-sddm/comar/package.py
+++ groni/kde5/kde5-plasma/kde5-sddm/comar/package.py
@@ -1,46 +1,20 @@
 #!/usr/bin/python
 
 import os, re
-import shutil
 
-OUR_ID = 65
-OUR_NAME = "sddm"
-OUR_DESC = "sddm"
-
-DATADIR = "/var/lib/sddm"
-DATADIRMODE = 0755
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-
-    # On first install...
-    if not os.path.exists(DATADIR):
-        os.makedirs(DATADIR, DATADIRMODE)
-
     try:
-        os.system ("groupadd -g %d %s" % (OUR_ID, OUR_NAME))
-        os.system ("useradd -m -d /var/lib/sddm -r -s /bin/false -u %d -g %d %s -c %s" % (OUR_ID, OUR_ID, OUR_NAME, OUR_DESC))
+        os.system ("groupadd --system sddm")
+        os.system ('useradd -c "Simple Desktop Display Manager" --system -d /var/lib/sddm -s /usr/bin/nologin -g sddm sddm')
         os.system ("passwd -l sddm > /dev/null")
-        os.system("/bin/chown -R sddm:sddm %s" % DATADIR)
+        os.system ("chown -R sddm:sddm /var/lib/sddm > /dev/null")
     except:
         pass
-
-# os.system ("groupadd --system %s" % (OUR_NAME))
-# os.system ("useradd -c 'sddm' --system -d /var/lib/sddm -s /sbin/nologin -g %s %s" % (OUR_NAME, OUR_DESC))
-    
-
-
-    # os.system("/bin/chown -R sddm:sddm /var/log/sddm.log")
-
-        
 
 def postRemove():
     try:
-        os.system ("userdel %s" % OUR_NAME)
-        os.system ("groupdel %s" % OUR_NAME)
+        os.system ("userdel sddm")
+        os.system ("groupdel sddm")
     except:
         pass
-
-
-
-
-
