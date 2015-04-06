088121addd6951f9d64d3851ec6619cac4bac264
aydemir <yusuf.aydemir@istanbul.com>
2015-04-07 01:49:47 +0300

    mesa

--- narcisse/Optimus/mesa/actions.py
+++ narcisse/Optimus/mesa/actions.py
@@ -10,50 +10,64 @@ from pisi.actionsapi import pisitools
 Libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib"
 
 def setup():
-    autotools.autoreconf("-vif")
+    #autotools.autoreconf("-vif")
 
 # --enable-sysfs option provides better hardware information support with "lspci"
 # --enable-32-bit option is not present anymore. Although build fails in emul32. With --disable-asm option, not fail. Needs to be tested.
 
     options ="\
+              --prefix=/usr \
+              --sysconfdir=/etc \
               --with-dri-driverdir=/usr/lib/xorg/modules/dri \
-              --with-gallium-drivers=r300,r600,nouveau,svga,swrast \
+              --with-gallium-drivers=r300,r600,radeonsi,nouveau,svga,swrast \
               --with-dri-drivers=i915,i965,r200,radeon,nouveau,swrast \
               --with-egl-platforms=x11,drm,wayland \
-              --enable-xa \
-              --enable-dri \
+              --enable-llvm-shared-libs \
               --enable-egl \
               --enable-gbm \
-              --enable-glx \
-              --enable-dri3 \
-              --enable-gles1 \
-              --enable-gles2 \
-              --enable-vdpau \
-              --enable-openvg \
-              --enable-osmesa \
-              --enable-sysfs \
-              --enable-xvmc \
-              --enable-glx-tls \
-              --enable-gallium-egl \
-              --enable-gallium-gbm \
               --enable-gallium-llvm \
               --enable-shared-glapi \
+              --enable-glx \
+              --enable-glx-tls \
+              --enable-dri \
+              --enable-osmesa \
+              --enable-gles1 \
+              --enable-gles2 \
               --enable-texture-float \
-             "
+              --enable-xa \
+              --enable-vdpau \
+              --enable-omx \
+              --enable-nine \
+              --enable-opencl"
 
     if get.buildTYPE() == "emul32":
         # compile with llvm doesn't work for now, test it later
-        
-             
-        options += " --with-dri-driverdir=/usr/lib32/xorg/modules/dri \
-                     --with-gallium-drivers=r600,nouveau,swrast \
-                     --with-clang-libdir=/usr/lib32 \
-                     --disable-gallium-llvm \
-                     --disable-asm \
-                     --enable-32-bit"
-                     
+
+        options += " --build=i686-pc-linux-gnu --host=i686-pc-linux-gnu \
+                     --libdir=/usr/lib32 \
+                     --prefix=/usr \
+                     --sysconfdir=/etc \
+                     --with-dri-driverdir=/usr/lib32/xorg/modules/dri \
+                     --with-gallium-drivers=r300,r600,radeonsi,nouveau,swrast \
+                     --with-dri-drivers=i915,i965,r200,radeon,nouveau,swrast \
+                     --with-egl-platforms=x11,drm,wayland \
+                     --enable-llvm-shared-libs \
+                     --enable-egl \
+                     --enable-gbm \
+                     --enable-gallium-llvm \
+                     --enable-shared-glapi \
+                     --enable-glx-tls \
+                     --enable-dri \
+                     --enable-glx \
+                     --enable-osmesa \
+                     --enable-gles1 \
+                     --enable-gles2 \
+                     --enable-texture-float \
+                     --enable-nine \
+                     --enable-vdpau"
+
     elif get.ARCH() == "x86_64":
-       
+
         options += " --with-clang-libdir=/usr/lib \
                      --enable-opencl-icd \
                    "
