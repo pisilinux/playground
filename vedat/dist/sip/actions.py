89cbf79cd78186600ecebcea177f9e0a946d7b22
vedat <vedat@pisilinux.org>
2015-05-26 22:22:20 +0300

    sip, sip3, pyqt5

--- vedat/dist/sip/actions.py
+++ vedat/dist/sip/actions.py
@@ -15,32 +15,16 @@ py2dir = get.curPYTHON()
 py3dir = "python3.4"
 
 def setup():
-    shelltools.cd("..")
-    shelltools.makedirs("build_python3")
-    shelltools.copytree("./%s" % WorkDir,  "build_python3")
-    shelltools.cd(WorkDir)
     pythonmodules.run('configure.py \
                     -b /usr/bin \
                     -d /usr/lib/%s/site-packages \
                     -e /usr/include/%s \
                     CFLAGS+="%s" CXXFLAGS+="%s"' % (py2dir, py2dir, get.CFLAGS(), get.CXXFLAGS()))
-
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    pythonmodules.run('configure.py \
-                    -b /usr/bin \
-                    -d /usr/lib/%s/site-packages \
-                    -e /usr/include/%s \
-                    CFLAGS="%s" CXXFLAGS="%s"' % (py3dir, py3dir, get.CFLAGS(), get.CXXFLAGS()), pyVer = "3")
-
+   
 def build():
     autotools.make()
-
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    autotools.make()
-
+    
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.remove("/usr/bin/sip")
     pisitools.dodoc("LICENSE*", "NEWS", "README")
-
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
