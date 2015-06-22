7c8c3f1d977f67714477df8f9499697bf2d95ce9
mathias-freire <mathiasfreire45@gmail.com>
2015-06-22 19:06:46 +0300

    libomxil-bellagio add 32bit

--- narcisse/Optimus/libomxil-bellagio/actions.py
+++ narcisse/Optimus/libomxil-bellagio/actions.py
@@ -1,21 +1,34 @@
+ 
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fiv")
-    autotools.configure("--disable-static")
+  
+    autotools.autoreconf("-vfi")
+  
+    if get.buildTYPE() == "emul32":
+      
+      autotools.configure("--libdir=/usr/lib32 \
+	                   --disable-werror") 
+      
+    elif get.ARCH() == "x86_64":
+      
+      autotools.configure("--disable-werror")    
 
 def build():
-    autotools.make()
+    
+    autotools.make("-j8")
 
 def install():
+    
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    
