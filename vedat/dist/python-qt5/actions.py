5539249daf9e00bf2e4d07feee6d13d4570b5ad8
Vedat Demir <vedat@pisilinux.org>
2015-09-14 22:16:52 +0300

    sip,dbus-python3,python-qt5

--- vedat/dist/python-qt5/actions.py
+++ vedat/dist/python-qt5/actions.py
@@ -14,38 +14,81 @@ WorkDir="PyQt-gpl-%s" % get.srcVERSION()
 
 def setup():
   
-    shelltools.copytree("%s/PyQt-gpl-5.4.1" % get.workDIR(), "%s/Py2Qt-gpl-5.4.1" % get.workDIR())
+    shelltools.copytree("%s/PyQt-gpl-5.5" % get.workDIR(), "%s/Py2Qt-gpl-5.5" % get.workDIR())
     pythonmodules.run("configure.py  --confirm-license \
                                      --assume-shared \
                                      --no-timestamp \
                                      --qsci-api \
-                                     --enable=QtPrintSupport \
-                                     --enable=QtWebKit \
-                                     --enable=QtWebKitWidgets \
-                                     --enable=QtMultimedia \
-                                     --enable=QtNetwork \
+                                     --enable=QtXmlPatterns \
+                                     --enable=QtSvg \
+                                     --enable=QtDBus \
+                                     --enable=QtQml \
+                                     --enable=QtQuick \
+                                     --enable=QtQuickWidgets \
+                                     --enable=QtOpenGL \
+                                     --enable=QtTest \
+                                     --enable=QtSql \
+                                     --enable=QtSensors \
                                      --enable=QtCore \
                                      --enable=QtWidgets \
+                                     --enable=QtWebKitWidgets \
                                      --enable=QtXml \
                                      --enable=QtGui \
+                                     --enable=QtNetwork \
+                                     --enable=QtWebKit \
+                                     --enable=QtPrintSupport \
+                                     --enable=QtX11Extras \
                                      --sip /usr/bin/sip \
                                      --qmake='/usr/lib/qt5/bin/qmake' \
                                      --destdir='/usr/lib/python3.4/site-packages' \
-                                     --sip-incdir='/usr/include/python3.4/' \
+                                     --sip-incdir='/usr/include/python3.4' \
                                      CFLAGS='%s' CXXFLAGS='%s'" % (get.CFLAGS(), get.CXXFLAGS()), pyVer = "3")
-       
-    shelltools.system("find -name 'Makefile' -exec sed -i 's|-Wl,-rpath,/usr/lib||g;s|-Wl,-rpath,.* ||g' {} \;")
     
+    shelltools.cd("%s/Py2Qt-gpl-5.5" % get.workDIR())
+    shelltools.system("python configure.py  --confirm-license \
+                                     --assume-shared \
+                                     --no-timestamp \
+                                     --qsci-api \
+                                     --enable=QtXmlPatterns \
+                                     --enable=QtSvg \
+                                     --enable=QtDBus \
+                                     --enable=QtQml \
+                                     --enable=QtQuick \
+                                     --enable=QtQuickWidgets \
+                                     --enable=QtOpenGL \
+                                     --enable=QtTest \
+                                     --enable=QtSql \
+                                     --enable=QtSensors \
+                                     --enable=QtCore \
+                                     --enable=QtWidgets \
+                                     --enable=QtWebKitWidgets \
+                                     --enable=QtXml \
+                                     --enable=QtGui \
+                                     --enable=QtNetwork \
+                                     --enable=QtWebKit \
+                                     --enable=QtPrintSupport \
+                                     --enable=QtX11Extras \
+                                     --destdir='/usr/lib/python2.7/site-packages' \
+                                     --sip-incdir='/usr/include/python2.7' \
+                                     --sip /usr/bin/sip \
+                                     --qmake='/usr/lib/qt5/bin/qmake'")
+  
+
 def build():
     
+    autotools.make()
+    shelltools.cd("%s/Py2Qt-gpl-5.5" % get.workDIR())
     autotools.make()
 
 def install():
   
+    autotools.rawInstall("DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
+    pisitools.rename("/usr/bin/pyuic5", "pyuic5-python3")
+    
+    shelltools.cd("%s/Py2Qt-gpl-5.5" % get.workDIR())
+    autotools.rawInstall("DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
     autotools.rawInstall("-C pyrcc DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
     autotools.rawInstall("-C pylupdate DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
-    autotools.rawInstall("DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
-      #pisitools.rename("/usr/bin/pyuic5", "pyuic5-python")
     
     pisitools.dohtml("doc/html/*")
     
--- vedat/dist/sip/actions.py
+++ vedat/dist/sip/actions.py
@@ -10,21 +10,43 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
+
 WorkDir = "%s-%s" % (get.srcNAME(),  get.srcVERSION())
 py2dir = get.curPYTHON()
 py3dir = "python3.4"
 
 def setup():
+    shelltools.cd("..")
+    shelltools.makedirs("build_python3")
+    shelltools.copytree("./%s" % WorkDir,  "build_python3")
+    shelltools.cd(WorkDir)
     pythonmodules.run('configure.py \
                     -b /usr/bin \
                     -d /usr/lib/%s/site-packages \
                     -e /usr/include/%s \
                     CFLAGS+="%s" CXXFLAGS+="%s"' % (py2dir, py2dir, get.CFLAGS(), get.CXXFLAGS()))
-   
+
+    shelltools.cd("../build_python3/%s" % WorkDir)
+    pythonmodules.run('configure.py \
+                    -b /usr/bin \
+                    -d /usr/lib/%s/site-packages \
+                    -e /usr/include/%s \
+                    CFLAGS="%s" CXXFLAGS="%s"' % (py3dir, py3dir, get.CFLAGS(), get.CXXFLAGS()), pyVer = "3")
+
 def build():
     autotools.make()
-    
+
+    shelltools.cd("../build_python3/%s" % WorkDir)
+    autotools.make()
+
 def install():
+
+    shelltools.cd("../build_python3/%s" % WorkDir)
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.remove("/usr/bin/sip")
+
+    shelltools.cd("..")
+    
+    shelltools.cd("../%s" % WorkDir)
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.dodoc("LICENSE*", "NEWS", "README")
