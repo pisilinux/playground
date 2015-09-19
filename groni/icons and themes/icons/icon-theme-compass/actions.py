24f5530421523cc147c6c9e5f11a68d7ef676f74
groni <groni3427@gmail.com>
2015-09-20 00:20:00 +0200

    icon theme compass for new kde

--- groni/icons and themes/icons/icon-theme-compass/actions.py
+++ groni/icons and themes/icons/icon-theme-compass/actions.py
@@ -4,11 +4,11 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-WorkDir = "./"
+#WorkDir = "./"
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def install():
-    pisitools.insinto("/usr/share/icons", "Compass-KDE")
+    pisitools.insinto("/usr/share/icons", "Compass")
