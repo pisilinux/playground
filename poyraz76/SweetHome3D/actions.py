66ec8df17598e854ed62d116d6f4ecfb680c45b4
groni <groni3427@gmail.com>
2016-02-23 16:06:21 +0100

    remove binary packages

--- poyraz76/SweetHome3D/actions.py
+++ poyraz76/SweetHome3D/actions.py
@@ -8,9 +8,14 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+#anthome = "/usr/bin/ant"
+#javadir = "/usr/lib/jvm/java-7-openjdk"
+
 def build():
+   # shelltools.system('find . -name "*.jar" -exec rm -f {} \;')
     shelltools.export("JAVA_HOME","/usr/lib/jvm/java-7-openjdk")
-    shelltools.system("ant")
+    #shelltools.system("ant")
+    shelltools.system("ant -buildfile.xml")
 
 def install():
     pisitools.insinto("/opt/SweetHome3D", "install/SweetHome3D-%s.jar" % get.srcVERSION())
