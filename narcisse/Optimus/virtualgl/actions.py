3c51fe1755be89675547ebe586984cc7598640fb
mathias-freire <mathiasfreire45@gmail.com>
2016-03-01 23:35:52 +0200

    virtualgl fix

--- narcisse/Optimus/virtualgl/actions.py
+++ narcisse/Optimus/virtualgl/actions.py
@@ -14,21 +14,26 @@ def setup():
     
     options = "-DCMAKE_INSTALL_PREFIX=/usr/share \
                -DTJPEG_INCLUDE_DIR=/usr/include \
-               -DTJPEG_LIBRARY=/usr/lib/libturbojpeg.so \
-               -DVGL_BINDIR=/usr/bin \
                -DVGL_INCDIR=/usr/include \
                -DVGL_DOCDIR=/usr/share/doc/%s \
+               -DVGL_SYSTEMFLTK=ON \
+               -DVGL_BUILDSTATIC=OFF \
                " % get.srcNAME()
     
     if get.buildTYPE() == "emul32":
       
       options += "-DVGL_LIBDIR=/usr/lib32 \
+                  -DTJPEG_LIBRARY=/usr/lib32/libturbojpeg.so \
                   -DVGL_FAKELIBDIR=/usr/lib32/fakelib \
+                  -DVGL_BINDIR=/usr/lib32/virtualgl/bin \
                  "
     elif get.ARCH() == "x86_64":
       
       options += "-DVGL_LIBDIR=/usr/lib \
+                  -DTJPEG_LIBRARY=/usr/lib/libturbojpeg.so \
                   -DVGL_FAKELIBDIR=/usr/lib/fakelib \
+                  -DVGL_BINDIR=/usr/bin \
+                  -DVGL_SYSTEMGLX=ON \
                  "
       
     
@@ -41,4 +46,16 @@ def build():
 def install():
     shelltools.cd("build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.domove("/usr/bin/glxinfo", "/usr/bin/", "vglxinfo")
+    
+    if get.buildTYPE() == "emul32":      
+      pisitools.domove("/usr/lib32/virtualgl/bin/glxinfo", "/usr/lib32/virtualgl/bin", "vglxinfo")    
+      
+      for exe in ["cpustat", "glreadtest", "glxspheres", "nettest", "tcbench",
+		  "vglclient", "vglconfig", "vglconnect", "vglgenkey", "vgllogin",
+		  "vglrun", "vglserver_config", "vglxinfo"]:
+	
+	pisitools.dosym("/usr/lib32/virtualgl/bin/%s" %exe, "/usr/bin/%s32" %exe)
+	
+    else:      
+      pisitools.rename("/usr/bin/glxinfo", "vglxinfo")  
+      pisitools.rename("/usr/bin/glxspheres64", "glxspheres")
